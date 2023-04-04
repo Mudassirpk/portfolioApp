9abe1f28460f1171d3e1569876a50e25ecddb8e5
Mudassirpk <mudassir.pk.mp@gmail.com>
2023-04-04 23:30:39 +0500

    latest commit f-55k

--- src/components/Main/Skills.jsx
+++ src/components/Main/Skills.jsx
@@ -2,20 +2,26 @@ function Skills() {
   return (
     <section className="skills" id="skills">
       <h1 className="text__lg">Skills</h1>
-      <p className="text__sm">
+      <p className="text__sm text_bold">
         Front End :{" "}
         <span className="text__red">
           html , css , javascript , reactjs , bootstrab and sass.
         </span>
       </p>
-      <p className="text__sm">
+      <p className="text__sm text_bold">
         Back End :{" "}
         <span className="text__red">
           NodeJS , ExpressJS , MongoDb and SanityIO.
         </span>
       </p>
-      <p className="text__sm">
-        Front End : <span className="text__red">AdobeXD and Figma.</span>
+      <p className="text__sm text_bold">
+        Design : <span className="text__red">AdobeXD and Figma.</span>
+      </p>
+      <p className="text__sm text_bold">
+        Technologies :{" "}
+        <span className="text__red">
+          Typescript, Websockets(SocketIO), Git(Github).
+        </span>
       </p>
     </section>
   );
